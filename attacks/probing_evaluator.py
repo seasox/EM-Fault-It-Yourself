8a3661d450954584051c2eee1628554f1a1380c4
Jeremy Boy <github@jboy.eu>
2023-07-19 11:02:54 +0200

    Overlay heatmap on chip Pass pickle fname and overlay as args to probing_evaluator add stm32f4 overlay image

--- attacks/probing_evaluator.py
+++ attacks/probing_evaluator.py
@@ -1,31 +1,58 @@
 import pickle
 import sys
+from typing import Optional, Dict, Any
 
-from Comm import STATUS
-#from probing import evaluate, Datapoint, Metric
 import matplotlib.pyplot as plt
 
+
+def usage():
+    print(f"Usage: {sys.argv[0]} $pickle [$board_overlay]")
+
+
 def main():
-    with open("/home/pi/pycharm_mnt/EM-Fault-It-Yourself/pickles/data_2023-06-28-14:12:31.pickle", "rb") as fp:
+    if len(sys.argv) < 2:
+        usage()
+        exit(1)
+    fname = sys.argv[1]
+    overlay = sys.argv[2] if len(sys.argv) > 2 else None
+    with open(fname, "rb") as fp:
         # TODO this might do stupid stuff use with caution
-        dps = pickle.load(fp)
+        from attacks.probing import Datapoint
+        dps: [Datapoint] = pickle.load(fp)
         dx = len(dps)
         dy = len(dps[0])
         dz = len(dps[0][0])
         perf_xy_planes = []
         for z in range(dz):
+            vis = {}
             perf = [[0 for _ in range(dy)] for _ in range(dx)]
             for x in range(dx):
                 for y in range(dy):
                     # the performances of the datapoints created at the location
+                    from attacks.probing import evaluate
+                    from attacks.probing import Metric
                     values = [evaluate(d, Metric.AnyFlipAnywhere) for d in dps[x][y][z]]
-                    perf[x][y] = max(values)
-            perf_xy_planes.append(perf)
-        visualize(perf_xy_planes[0])
+                    perf[x][y] = max(values or [0])
+            vis["perf"] = perf
+            vis["title"] = f"{Metric.AnyFlipAnywhere}; Layer {80 + dps[0][0][z][0].attack_location[2]}"
+            perf_xy_planes.append(vis)
+        for plane in perf_xy_planes:
+            visualize(plane, overlay)
 
-def visualize(xy_plane):
-    plt.imshow(xy_plane, cmap='viridis', interpolation='nearest')
-    plt.colorbar()
+
+def visualize(vis: Dict[str, Any], overlay: Optional[str]):
+    import numpy as np
+    matrix = np.array(vis["perf"])
+    fig, ax = plt.subplots()
+    ax.xaxis.tick_top()
+    if overlay:
+        im = plt.imread(overlay)
+        im = np.flipud(im)
+        ax.imshow(im, extent=[-1, matrix.shape[1], -1, matrix.shape[0]])
+    heatmap = ax.imshow(matrix, cmap='viridis', alpha=0.5, interpolation='nearest')
+    ax.set_aspect('equal')
+    plt.title(vis["title"])
+    fig.colorbar(heatmap)
     plt.show()
 
 
