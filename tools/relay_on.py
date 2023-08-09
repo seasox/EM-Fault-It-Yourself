2d5c021141a5765532826365db5533e90024ade0
Jeremy Boy <github@jboy.eu>
2023-08-09 21:06:33 +0200

    add constants for relay states

--- tools/relay_on.py
+++ tools/relay_on.py
@@ -3,9 +3,11 @@ from RPi import GPIO
 
 def main():
     reset_pin = 0
+    RELAY_STATE_ON = 0
+    RELAY_STATE_OFF = 1
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(reset_pin, GPIO.OUT)
-    GPIO.output(reset_pin, 0)
+    GPIO.output(reset_pin, RELAY_STATE_ON)
 
 
 if __name__ == '__main__':
