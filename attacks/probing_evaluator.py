f8c7cf72ecc293660273cd29e24f9e3ac7fd6570
Jeremy Boy <github@jboy.eu>
2023-08-09 21:44:11 +0200

    refactor make_heatmap in probing_evaluator.py, fix axles in dps by swapping x and y (numpy arrays are rows times cols, but the array was cols (i.e. x) times rows (i.e. y))

--- attacks/probing_evaluator.py
+++ attacks/probing_evaluator.py
@@ -2,6 +2,9 @@ import pickle
 import sys
 from typing import Optional, Dict, Any
 
+from attacks.probing import Datapoint
+from attacks.probing import Metric
+
 
 def usage():
     print(f"Usage: {sys.argv[0]} $pickle [$board_overlay]")
@@ -29,31 +32,44 @@ def main():
     fname = sys.argv[1]
     overlay = sys.argv[2] if len(sys.argv) > 2 else None
     with open(fname, "rb") as fp:
-        # TODO this might do stupid stuff use with caution
-        from attacks.probing import Datapoint
-        dps: [Datapoint] = pickle.load(fp)
-        dx = len(dps)
-        dy = len(dps[0])
-        dz = len(dps[0][0])
-        perf_xy_planes = []
-        for z in range(dz):
-            vis = {}
-            perf = [[0.0 for _ in range(dy)] for _ in range(dx)]
-            from attacks.probing import evaluate
-            from attacks.probing import Metric
-            for x in range(dx):
-                for y in range(dy):
-                    # the performances of the datapoints created at the location
-                    values = [evaluate(d, Metric.AnyFlipAnywhere) for d in dps[x][y][z]]
-                    perf[x][y] = max(values or [0])
-            vis["perf"] = perf
-            vis["title"] = f"{Metric.AnyFlipAnywhere}; Layer {80 + dps[0][0][z][0].attack_location[2]}"
-            perf_xy_planes.append(vis)
-        for plane in perf_xy_planes:
-            visualize(plane, overlay)
+        def max_cb(values):
+            return max(values or [0])
+
+        def success_rate_cb(values):
+            return len(list(filter(lambda p: p > 0, values))) / len(values)
+
+        def crash_rate_cb(values):
+            return len(list(filter(lambda p: p < 0, values))) / len(values)
+
+        dps: [Datapoint] = pickle.load(fp)  # TODO implement JSON import/export. Pickles are kinda slow
+        make_heatmap(dps, overlay, discrete_cmap(10, 'Greys'), Metric.AnyFlipAnywhere, "Max Score", max_cb)
+        make_heatmap(dps, overlay, discrete_cmap(10, 'Greens'), Metric.AnyFlipAnywhere, "Pr[#flip > 0]",
+                     success_rate_cb)
+        make_heatmap(dps, overlay, discrete_cmap(10, 'Reds'), Metric.AnyFlipAnywhere, "Pr[crash]", crash_rate_cb)
 
 
-def visualize(vis: Dict[str, Any], overlay: Optional[str]):
+def make_heatmap(dps: [[[Datapoint]]], overlay, cmap, metric, title, callback):
+    dx = len(dps)
+    dy = len(dps[0])
+    dz = len(dps[0][0])
+    perf_xy_planes = []
+    for z in range(dz):
+        vis = {}
+        perf = [[0.0 for _ in range(dx)] for _ in range(dy)]
+        from attacks.probing import evaluate
+        for x in range(dx):
+            for y in range(dy):
+                # the performances of the datapoints created at the location
+                values = [evaluate(d, metric) for d in dps[x][y][z]]
+                perf[y][x] = callback(values)
+        vis["perf"] = perf
+        vis["title"] = f"{title}; {metric}; Layer {83 + dps[0][0][z][0].attack_location[2]}"
+        perf_xy_planes.append(vis)
+    for plane in perf_xy_planes:
+        visualize(plane, overlay, cmap)
+
+
+def visualize(vis: Dict[str, Any], overlay: Optional[str], cmap):
     import matplotlib.pyplot as plt
     import numpy as np
     matrix = np.array(vis["perf"])
@@ -63,7 +79,7 @@ def visualize(vis: Dict[str, Any], overlay: Optional[str]):
         im = plt.imread(overlay)
         im = np.flipud(im)
         ax.imshow(im, extent=[-1, matrix.shape[1], -1, matrix.shape[0]])
-    heatmap = ax.imshow(matrix, cmap=discrete_cmap(10, 'Greens'), alpha=0.5, interpolation='nearest')
+    heatmap = ax.imshow(matrix, cmap=cmap, alpha=0.5, interpolation='nearest')
     ax.set_aspect('equal')
     plt.title(vis["title"])
     fig.colorbar(heatmap)
