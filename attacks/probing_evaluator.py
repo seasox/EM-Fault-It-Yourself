12ea99f37a0efa61b1eed6370fd59717cd6f0bfd
Jeremy Boy <github@jboy.eu>
2024-04-04 16:58:13 +0200

    adapt probing_evaluator.py for updated probing outputs

--- attacks/probing_evaluator.py
+++ attacks/probing_evaluator.py
@@ -1,6 +1,9 @@
 import json
 import sys
+import typing as t
 from typing import Optional, Dict, Any
+
+import numpy as np
 
 from attacks.Comm import DatapointDecoder
 from attacks.probing import Datapoint
@@ -26,6 +29,24 @@ def discrete_cmap(N, base_cmap=None):
     return base.from_list(cmap_name, color_list, N)
 
 
+def max_cb(values):
+    return max(values or [0])
+
+
+def avg_cb(values):
+    non_crash = [value for value in values if value >= 0] or [0]
+    return sum(non_crash) / len(non_crash) if len(non_crash) > 0 else 0
+
+
+def success_rate_cb(values):
+    return len(list(filter(lambda p: p > 0, values))) / len(values) if len(values) > 0 else 0
+
+
+def sub_tuples(a, b) -> t.Tuple:
+    assert (len(a) == len(b))
+    return tuple([int(a[i] - b[i]) for i in range(len(a))])
+
+
 def main():
     if len(sys.argv) < 2:
         usage()
@@ -33,19 +54,31 @@ def main():
     fname = sys.argv[1]
     overlay = sys.argv[2] if len(sys.argv) > 2 else None
 
-    def max_cb(values):
-        return max(values or [0])
-
-    def avg_cb(values):
-        non_crash = [value for value in values if value >= 0] or [0]
-        return sum(non_crash) / len(non_crash) if len(non_crash) > 0 else 0
-
-    def success_rate_cb(values):
-        return len(list(filter(lambda p: p > 0, values))) / len(values) if len(values) > 0 else 0
-
     with open(fname, "r") as fp:
         experiment: dict[str] = json.load(fp, cls=DatapointDecoder)
-    dps: [[[Datapoint]]] = experiment['datapoints'] if 'datapoints' in experiment else experiment
+    if 'settings' in experiment:
+        # prepare DP array with offsets
+        start = experiment['settings']['experiment']['start']
+        start_offset = experiment['settings']['experiment']['start_offset']
+        end = experiment['settings']['experiment']['end']
+        end_offset = experiment['settings']['experiment']['end_offset']
+        start = sub_tuples(start, start_offset)
+        end = sub_tuples(end, end_offset)
+        dims = sub_tuples(end, start)
+        reps = experiment['settings']['experiment']['repetitions'] if 'repetitions' in experiment['settings'][
+            'experiment'] else 1000
+        dims = tuple([int(d) for d in dims] + [reps])
+        dims = sub_tuples(dims, (-1, -1, -1, 0))
+        dps = np.empty(dims, dtype=Datapoint)
+        start_x, start_y, start_z = start_offset
+        measurements = np.array(experiment['datapoints'])
+        shape = measurements.shape
+        end_x = start_x + shape[0]
+        end_y = start_y + shape[1]
+        end_z = start_z + shape[2]
+        dps[start_x:end_x + 1, start_y:end_y, start_z:end_z + 1] = measurements  # TODO off-by-one?
+    else:
+        dps: [[[Datapoint]]] = experiment
     make_heatmap(dps, overlay, discrete_cmap(10, 'Greys'),
                  Metric.AnyFlipAnywhere, "Any Flip Anywhere Average Score", avg_cb)
     make_heatmap(dps, overlay, discrete_cmap(10, 'Greens'), Metric.ZeroOneFlipOnR4OrR5, "Pr[0-1 R4 R5 > 0]",
@@ -63,7 +96,7 @@ def main():
     #             success_rate_cb)
 
 
-def make_heatmap(dps: [[[Datapoint]]], overlay, cmap, metric, title, callback):
+def make_heatmap(dps: np.typing.NDarray[Datapoint], overlay, cmap, metric, title, callback):
     dx = len(dps)
     dy = len(dps[0])
     dz = len(dps[0][0])
@@ -75,10 +108,13 @@ def make_heatmap(dps: [[[Datapoint]]], overlay, cmap, metric, title, callback):
         for x in range(dx):
             for y in range(dy):
                 # the performances of the datapoints created at the location
-                values = [evaluate(d, metric) for d in dps[x][y][z]]
-                perf[y][x] = callback(values)
+                if isinstance(dps[x][y][z], Datapoint):
+                    values = [evaluate(d, metric) for d in dps[x][y][z]]
+                    perf[y][x] = callback(values)
+                else:
+                    perf[y][x] = 0
         vis["perf"] = perf
-        vis["title"] = f"{title}; {metric}; Z={dps[0][0][z][0].attack_location[2]}"
+        vis["title"] = f"{title}; {metric}; Z={z}"
         perf_xy_planes.append(vis)
     for plane in perf_xy_planes:
         visualize(plane, overlay, cmap)
